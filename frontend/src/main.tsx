a9807757b3bba3b83d3214de8ff7233c0d89c2b8
mike16345 <95699872+mike16345@users.noreply.github.com>
2025-03-02 16:06:04 +0200

    Implemented theme provider from Material UI

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,25 @@
 import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import { SnackbarProvider } from "./context/SnackBarProvider.tsx";
+import { createTheme, ThemeProvider } from "@mui/material";
+import CssBaseline from "@mui/material/CssBaseline";
 import "./index.css";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-import App from "./App.tsx";
-import { SnackbarProvider } from "./context/SnackBarProvider.tsx";
+
+const theme = createTheme({
+  colorSchemes: {
+    dark: true,
+  },
+});
 
 createRoot(document.getElementById("root")!).render(
-  <SnackbarProvider>
-    <App />
-  </SnackbarProvider>
+  <ThemeProvider theme={theme}>
+    <SnackbarProvider>
+      <CssBaseline enableColorScheme />
+      <App />
+    </SnackbarProvider>
+  </ThemeProvider>
 );
