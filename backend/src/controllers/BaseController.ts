e43072ae2f4b292fc7217070e4ca4896e83f45b1
mike16345 <95699872+mike16345@users.noreply.github.com>
2025-03-02 18:14:46 +0200

    updated base controller to bind methods of any controller extending it

--- backend/src/controllers/BaseController.ts
+++ backend/src/controllers/BaseController.ts
@@ -1,6 +1,18 @@
 import { Response } from "express";
 
 export default class BaseController {
+  constructor() {
+    this.bindMethods();
+  }
+
+  private bindMethods() {
+    Object.getOwnPropertyNames(Object.getPrototypeOf(this)).forEach((key) => {
+      if (key !== "constructor") {
+        this[key] = this[key].bind(this);
+      }
+    });
+  }
+
   success(res: Response, data: any, message: string = "Success", statusCode: number = 200): any {
     return res.status(statusCode).json({
       success: true,
